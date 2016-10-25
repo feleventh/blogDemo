af22551c9576b5ee6e3acd9f4d056baa5e130746
feleventh <feleventh@hotmail.com>
2016-10-25 15:28:07 +0800

    firworks

---
+++ assets/js/test2.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2016/10/19.
+ */
+var Student = (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+document.body.innerHTML = greeter(user);
+//# sourceMappingURL=test2.js.map
